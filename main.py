5842e22b1ddabe5c4af0e7d785dd9b5574e4b4c1
TelesaludAI <telesalud.ai.ucc@gmail.com>
2025-12-16 23:39:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,56 @@ app.add_middleware(
 DATABASE_URL = os.environ["DATABASE_URL"]
 API_KEY = os.environ["API_KEY"]
 
+#class FormData(BaseModel):
+#    nombre: str
+#    correo: str | None = None
+#    celular: str | None = None
+#    mensaje: str | None = None
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import date, time
+
+
 class FormData(BaseModel):
+    # Identificación
+    id: str
+
+    # Fechas
+    fecha: date
+    fecha_cumpleanos: Optional[date] = None
+    fecha_registro: date
+
+    # Ubicación
+    municipio: str
+    barrio: Optional[str] = None
+
+    # Datos personales
     nombre: str
-    correo: str | None = None
-    celular: str | None = None
-    mensaje: str | None = None
+    cedula: str
+    correo: Optional[EmailStr] = None
+    celular: Optional[str] = None
+    profesion: Optional[str] = None
+    cargo: Optional[str] = None
+
+    # Campaña
+    lider: Optional[str] = None
+    referenciado_por: Optional[str] = None
+    sector: Optional[str] = None
+    etapa_campana: Optional[str] = None
+    confirmacion_voto: Optional[str] = None
+
+    # Votación
+    punto_votacion: Optional[str] = None
+    mesa_votacion: Optional[int] = None
+
+    # Geolocalización
+    lat_lon: Optional[str] = None
+
+    # Auditoría
+    useremail: EmailStr
+    hora_registro: time
+
 
 @app.post("/formulario")
 def guardar_formulario(
